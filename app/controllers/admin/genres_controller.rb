d2fa926767ca0747a0e57991cf13cccb35b7918d
EC2 Default User <ec2-user@ip-172-31-6-22.ec2.internal>
2022-02-19 06:26:00 +0000

    [finish]ECサイト作成

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,18 +1,15 @@
 class Admin::GenresController < ApplicationController
-before_action :authenticate_admin!, except:[:top]
-  def create
-    @genre = Genre.new(genre_params)
-    if @genre.save
-       redirect_to admin_genres_path
-    else
-       @genres = Genre.all
-       render :index
-    end
+before_action :authenticate_admin!
+  def index
+    @new_genre = Genre.new
+    @genres = Genre.all
   end
 
-  def index
-    @genre = Genre.new
-    @genres = Genre.all
+  def create
+    @new_genre = Genre.new(genre_params)
+    @new_genre.save
+    redirect_to admin_genres_path
+
   end
 
   def edit
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,5 @@
 class ApplicationController < ActionController::Base
 
-  def after_sign_up_path_for(resource)
-     customers_my_page_path
-  end
-
    def after_sign_in_path_for(resource)
      root_path
   end
